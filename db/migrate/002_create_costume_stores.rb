00a5978db6e2c10ff096db2f946a93773378ba01
Brian Nicholls <nichol88@gmail.com>
2020-01-24 04:34:33 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,6 +1,13 @@
 # Create your costume_stores migration here
 class CreateCostumeStores
   def change
-
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :number_of_costumes
+      t.integer :number_of_employees
+      t.timestamp :opening_time
+      t.timestamp :closing_time
+    end
   end
 end
