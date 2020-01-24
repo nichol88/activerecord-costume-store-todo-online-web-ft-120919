77560bb1b3ba413c7ec5c6d032f6d9924cb02a69
Brian Nicholls <nichol88@gmail.com>
2020-01-24 04:23:26 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,4 +9,13 @@
 # value types according to the spec
 
 class CreateCostumes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.size
+      t.image_url
+    end
+
+  end
 end
