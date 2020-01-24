b648aac23fa08c0bddf61cd43ee92cee14bef5e7
Brian Nicholls <nichol88@gmail.com>
2020-01-24 04:42:25 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,7 +4,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :number_of_costumes
+      t.integer :costume_inventory
       t.integer :number_of_employees
       t.boolean :in_business?
       t.timestamp :opening_time
