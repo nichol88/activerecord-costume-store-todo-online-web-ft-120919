6332bba3b5a58ad05b5cd83e0510cb9db1070afe
Brian Nicholls <nichol88@gmail.com>
2020-01-24 04:23:44 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,8 +13,8 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
     create_table :costumes do |t|
       t.string :name
       t.float :price
-      t.size
-      t.image_url
+      t.string :size
+      t.string :image_url
     end
 
   end
