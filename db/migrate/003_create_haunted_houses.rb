4c266bf049fe77980c0230533da3df39b50adf9c
Brian Nicholls <nichol88@gmail.com>
2020-01-24 04:34:52 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,13 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.integer :number_of_costumes
+      t.integer :number_of_employees
+      t.timestamp :opening_time
+      t.timestamp :closing_time
+    end
+  end
 end
